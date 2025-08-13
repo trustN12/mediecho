3ce6fea6c6861aee450959422da501cd248d659c
NABARUN <nbiswas100@outlook.com>
2025-08-13 23:57:42 +0530

    revert back to default next config

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,27 @@
 
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-  
-// };
-
-// export default nextConfig
-
-
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.mediecho.in' }],
-        destination: 'https://mediecho.in/:path*',
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
+  
 };
 
-export default nextConfig;
+export default nextConfig
+
+
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//   async redirects() {
+//     return [
+//       {
+//         source: '/:path*',
+//         has: [{ type: 'host', value: 'www.mediecho.in' }],
+//         destination: 'https://mediecho.in/:path*',
+//         permanent: true,
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
